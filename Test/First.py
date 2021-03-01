af7e1683b59072dc1f367829d59f50a74dd37ce9
Mu-il Yang <muilyang12@gmail.com>
2021-03-01 18:36:19 +0900

    Real Start

--- Test/First.py
+++
@@ -1,7 +0,0 @@
-print("Hello, Mu-il")
-
-## Good to hear from you, man.
-## Good to hear from you, man.
-## Good to hear from you, man.
-## Good to hear from you, man.
-## Good to hear from you, man.
